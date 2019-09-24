1061fe2bbbfbe95a598ed2f28545f0c08f9713e7
Akshay Mule <muleakshay29@gmail.com>
2019-09-24 13:58:20 +0530

    updated common master child api

--- src/routers/frm-common-master-child.js
+++ src/routers/frm-common-master-child.js
@@ -24,4 +24,74 @@ router.get("/fetch-commonmaster-child", (req, res) => {
     });
 });
 
+router.post("/check-cmcname", (req, res) => {
+  CommonMasterChild.find({ CMC_Name: req.body.CMC_Name })
+    .then(ccmaster => {
+      res.send(ccmaster);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
+
+router.get("/fetch-commonmasterchild-details/:id", (req, res) => {
+  const _id = req.params.id;
+
+  CommonMasterChild.findById(_id)
+    .then(ccmaster => {
+      if (!ccmaster) {
+        return res.status(404).send();
+      }
+
+      res.status(200).send(ccmaster);
+    })
+    .catch(e => {
+      res.status(500).send(e);
+    });
+});
+
+router.patch("/update-commonmaster-child/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["CMC_Name"];
+  const isValidOperation = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const commonMasterChild = await CommonMasterChild.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!commonMasterChild) {
+      res.status(404).send();
+    }
+
+    res.send(commonMasterChild);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.delete("/commonmasterchild/:id", async (req, res) => {
+  try {
+    const commonMasterChild = await CommonMasterChild.findByIdAndDelete(
+      req.params.id
+    );
+
+    if (!commonMasterChild) {
+      return res.status(404).send();
+    }
+
+    res.send(commonMasterChild);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
